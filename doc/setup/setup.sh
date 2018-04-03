65910038b494708300a6dfd337fdf557a74e7bbc
ELAIN <tristan.elain@sogeti.com>
2018-04-03 14:31:20 +0200

    ajout de choix de config client/server/bootstrap

--- doc/setup/setup.sh
+++ doc/setup/setup.sh
@@ -13,43 +13,43 @@ then
 fi
 unzip=`which unzip`
 if [[ $unzip == "" ]]; then
-        echo -e "Ce progamme nécessite la présence du paquet unzip. Veuillez l'installer"
-        exit 1
+	echo -e "Ce progamme nécessite la présence du paquet unzip. Veuillez l'installer"
+	exit 1
 fi
 
 #Si consul n'est pas présent dans le path, on l'installe
 program=`which consul`
 if [[ -z $program ]]; then
-        echo "Installation de consul"
-        architecture=`uname -m`
+	echo "Installation de consul"
+	architecture=`uname -m`
 
-        if [[ $architecture == *"arm"* ]]; then
-                arm_version=${architecture:3:1}
-                if [[ arm_version -le 7 ]]; then #arm 32bits
-                        LINK="https://releases.hashicorp.com/consul/1.0.2/consul_1.0.2_linux_arm.zip"
-                else #arm 64bits
-                        LINK="https://releases.hashicorp.com/consul/1.0.2/consul_1.0.2_linux_arm64.zip"
-                fi
+	if [[ $architecture == *"arm"* ]]; then
+		arm_version=${architecture:3:1}
+		if [[ arm_version -le 7 ]]; then #arm 32bits
+			LINK="https://releases.hashicorp.com/consul/1.0.2/consul_1.0.2_linux_arm.zip"
+		else #arm 64bits
+			LINK="https://releases.hashicorp.com/consul/1.0.2/consul_1.0.2_linux_arm64.zip"
+		fi
 
-        elif [[ $architecture == *"64" ]]; then #architecture 64bits
-                LINK="https://releases.hashicorp.com/consul/1.0.2/consul_1.0.2_linux_amd64.zip"
+	elif [[ $architecture == *"64" ]]; then #architecture 64bits
+		LINK="https://releases.hashicorp.com/consul/1.0.2/consul_1.0.2_linux_amd64.zip"
 
-        else #architecture 32bits
-                LINK="https://releases.hashicorp.com/consul/1.0.2/consul_1.0.2_linux_386.zip"
-        fi
+	else #architecture 32bits
+		LINK="https://releases.hashicorp.com/consul/1.0.2/consul_1.0.2_linux_386.zip"
+	fi
 
-        DIR=`pwd`
+	DIR=`pwd`
 
-        cd /usr/local/bin
+	cd /usr/local/bin
 
-        wget $LINK
+	wget $LINK
 
-        unzip *.zip
+	unzip *.zip
 
-        rm -f *.zip
+	rm -f *.zip
 
-        cd $DIR
-        echo "done"
+	cd $DIR
+	echo "done"
 fi
 # Selection du nom de l'agent
 printf "nom de l'agent au sein du cluster: "
@@ -66,47 +66,35 @@ read -e ip_servers_tmp
 ip_array=( $ip_servers_tmp )
 IP_SERVERS=""
 for i in "${ip_array[@]}"; do
-        if [[ $IP_SERVERS != "" ]]; then IP_SERVERS=$IP_SERVERS", "; fi
-        IP_SERVERS=$IP_SERVERS"\""$i"\""
+    if [[ $IP_SERVERS != "" ]]; then IP_SERVERS=$IP_SERVERS", "; fi
+    IP_SERVERS=$IP_SERVERS"\""$i"\""
 done
 
-# Demande si activation de l'ui pour le client
-printf "Activation de l'UI pour la config client ? [O/n] : "
-read -e ui_response
-while [[ "$ui_response" != "n" && "$ui_response" != "o" ]]; do
-        printf "Entrée incorrecte. Entrer o ou n : "
-        read -e ui_response
-        echo $ui_response
+# Demande si config server ou client
+printf "Configuration Serveur ou Client ? [S/c] : "
+read -e config_response
+while [[ "$config_response" != "s" && "$config_response" != "c" ]]; do
+	printf "Ent incorrecte. Entrer s ou c. "
+	read -e config_response
 done
 
-if [[ "$ui_response" == "o" ]]; then
-        enable_ui="true"
-fi
+if [[ "$config_response" == "c" ]]; then
+	# Demande si activation de l'ui pour le client
+	printf "Activation de l'UI pour la config client ? [O/n] : "
+	read -e ui_response
+	while [[ "$ui_response" != "n" && "$ui_response" != "o" ]]; do
+		printf "Entrée incorrecte. Entrer o ou n. "
+		read -e ui_response
+	done
 
-# répertoires de conf
-mkdir -p /etc/consul.d/{bootstrap,server,client}
-mkdir -p $data_dir
-echo "{
-    \"bootstrap\": true,
-    \"server\": true,
-    \"datacenter\": \"$datacenter\",
-    \"data_dir\": \"$data_dir\",
-    \"log_level\": \"$log_level\",
-    \"enable_syslog\": $enable_syslog,
-        \"node_name\":\"$NAME\" }" > /etc/consul.d/bootstrap/config.json
-
-echo "{
-    \"bootstrap\": false,
-    \"server\": true,
-    \"datacenter\": \"$datacenter\",
-    \"data_dir\": \"$data_dir\",
-    \"log_level\": \"$log_level\",
-    \"enable_syslog\": $enable_syslog,
-    \"start_join\":[$IP_SERVERS],
-        \"bind_addr\": \"$IP_ADDR\",
-        \"node_name\":\"$NAME\" }" > /etc/consul.d/server/config.json
-
-echo "{
+	if [[ "$ui_response" == "o" ]]; then
+		enable_ui="true"
+	fi
+	
+	# Configuration client
+	mkdir -p /etc/consul.d/client
+	
+	echo "{
     \"server\": false,
     \"datacenter\": \"$datacenter\",
     \"data_dir\": \"$data_dir\",
@@ -114,7 +102,48 @@ echo "{
     \"log_level\": \"$log_level\",
     \"enable_syslog\": $enable_syslog,
     \"start_join\": [$IP_SERVERS],
+	\"bind_addr\": \"$IP_ADDR\",
+	\"node_name\":\"$NAME\" }" > /etc/consul.d/client/config.json
+	
+else
+	# Demande si config bootstrap ou non
+	printf "Mettre en place la configuration bootstrap ? [0/n] : "
+	read -e bs_response
+	while [[ "$bs_response" != "n" && "$bs_response" != "o" ]]; do
+		printf "Entrée incorrecte. Entrer o ou n. "
+		read -e bs_response
+	done
+	
+	# Configuration Bootstrap
+	if [[ "$bs_response" == "o" ]]; then
+		mkdir -p /etc/consul.d/bootstrap
+		echo "{
+			\"bootstrap\": true,
+			\"server\": true,
+			\"datacenter\": \"$datacenter\",
+			\"data_dir\": \"$data_dir\",
+			\"log_level\": \"$log_level\",
+			\"enable_syslog\": $enable_syslog,
+			\"node_name\":\"$NAME\" }" > /etc/consul.d/bootstrap/config.json
+	fi
+	
+	# Configuration serveur
+	mkdir -p /etc/consul.d/server
+	echo "{
+		\"bootstrap\": false,
+		\"server\": true,
+		\"datacenter\": \"$datacenter\",
+		\"data_dir\": \"$data_dir\",
+		\"log_level\": \"$log_level\",
+		\"enable_syslog\": $enable_syslog,
+		\"start_join\":[$IP_SERVERS],
         \"bind_addr\": \"$IP_ADDR\",
-        \"node_name\":\"$NAME\" }" > /etc/consul.d/client/config.json
+        \"node_name\":\"$NAME\" }" > /etc/consul.d/server/config.json
+
+fi
+
+# Répertoire de persistance
+mkdir -p $data_dir
+
 echo "Setup terminé"
 
